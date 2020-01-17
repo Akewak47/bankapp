09ad884514d96b6e21f7e76e43f8d2178143f2c1
akewak deyassa <akewakdeyassa0410@Gmail.com>
2020-01-17 15:02:24 -0600

    intial commit

---
+++ ConsoleApp1/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Account
+    {   
+        private int lastAccountNumber = 0;
+        #region Constructor 
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+        #endregion
+        #region properties 
+        public int AccountNumber { get; private set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; private set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        #endregion
+
+        #region methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance; 
+        }
+        #endregion
+
+        #region Constructor 
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            myAccount.AccountNumber = 12345;
+            myAccount.AccountName = "Akewak";
+            myAccount.AccountType = "checking";
+            myAccount.Deposit(1001);
+            Console.WriteLine($"A.Name: {myAccount.AccountName} AN: {myAccount.AccountNumber}, Balance: {myAccount.Balance}");
+        }
+    }
+}
